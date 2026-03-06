673fb25d35d7f30d4dc4da4044f17c793d96e81c
ElbekYusupov-1 <elbekbusnes@gmail.com>
2026-03-06 11:26:17 +0500

    Add files via upload

---
+++ work_12.py
@@ -0,0 +1,49 @@
+#Boolean 11
+# A=int(input())
+# B=int(input())
+# a=A%2==0 and B%2==0 or A%2==1 and B%2==1
+# print(a)
+#Boolean 12
+# A=int(input())
+# B=int(input())
+# C=int(input())
+# a=A>0 and B>0 and C>0
+# print(a)
+#Boolean 13
+# A=int(input())
+# B=int(input())
+# C=int(input())
+# a=A>0 or B>0 or C>0
+# print(a)
+#Boolean 14
+# A=int(input())
+# B=int(input())
+# C=int(input())
+# a=(A>0 and B<0 and C<0) or (A<0 and B>0 and C<0) or (A<0 and B<0 and C>0)
+# print(a)
+#Boolean 15
+# A=int(input())
+# B=int(input())
+# C=int(input())
+# a=(A>0 and B>0 and C<0) or (A<0 and B>0 and C>0) or (A>0 and B<0 and C>0)
+# print(a)
+#Boolean 16
+# a=int(input())
+# if 10<=a<=99 and a%2==0:
+#     print("Ikki xonlai juft son")
+# elif 10<=a<=99 and a%2==1:
+#     print("ikki xonali toq son")
+# else:
+#     print("Sonni do'g'ri bar maymun")
+# Boolean 17
+# a=int(input())
+# t1=100<=a<=999 and a%2==0:
+# print("uch xonali juft son")
+# t2=100<=a<=999 and a%2==1:
+# print("uch xonlai toq son")
+# Boolean 18
+# a,b,c=map(int, input().split())
+# if a==b or b==c or a==c:
+#     print("hech bo'masa ikkisi dangqu")
+# else:
+#     print("dangmas akanqu")
